4b155a05342644838a7602c4d746894150ea767b
Yanina Bustos <yanibustos4596@gmail.com>
2025-04-04 21:02:48 -0300

    Add routes

---
+++ routes.jsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter } from "react-router-dom";
+import ProtectedRoute from "./src/assets/components/ProtectedRoute/ProtectedRoute";
+import Dashboard from "./src/assets/pages/Dashboard/Dashboard";
+import Layout from "./src/assets/components/Layout/Layout";
+import Users from "./src/assets/pages/Users/Users";
+import Admin from "./src/assets/pages/Admin/Admin";
+import Products from "./src/assets/pages/Products/Products";
+import Orders from "./src/assets/pages/Orders/Orders";
+import Categories from "./src/assets/pages/Categories/Categories";
+import Roles from "./src/assets/pages/Roles/Roles";
+import NotFound from "./src/assets/pages/NotFound/NotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/admin",
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: "/admin",
+        element: <Layout />,
+        children: [
+          { path: "", element: <Dashboard /> },
+          { path: "users", element: <Users /> },
+          { path: "admin", element: <Admin /> },
+          { path: "products", element: <Products /> },
+          { path: "orders", element: <Orders /> },
+          { path: "categories", element: <Categories /> },
+          { path: "roles", element: <Roles /> },
+        ],
+      },
+    ],
+  },
+
+  { path: "*", element: <NotFound /> },
+]);
+
+export default router;
