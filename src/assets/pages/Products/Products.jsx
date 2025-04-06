5513307b1c5f5dc25f934aa77d7a12424e63234d
Yanina Bustos <yanibustos4596@gmail.com>
2025-04-05 22:49:19 -0300

    Products - Add component, styles and api call

--- src/assets/pages/Products/Products.jsx
+++ src/assets/pages/Products/Products.jsx
@@ -1,7 +1,107 @@
+import { useEffect, useState } from "react";
+import RemoveModal from "../../components/Modals/RemoveModal";
 import "./Products.css";
+import { toast } from "react-toastify";
+import fetchApi from "../../../api/fetchApi";
+import { Link } from "react-router-dom";
 
 function Products() {
-  return <div>Products</div>;
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const [showRemove, setShowRemove] = useState(false);
+  const handleCloseRemove = () => setShowRemove(false);
+  const handleShowRemove = () => setShowRemove(true);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    try {
+      const data = await fetchApi({ method: "get", url: "/products" });
+      console.log(data);
+      setProducts(data);
+    } catch (err) {
+      setError(err.message);
+      toast.error("Failed to load products.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleRemoveProduct = (productId) => {
+    console.log(`Deleted ${productId}`);
+    handleCloseRemove();
+  };
+
+  return (
+    <div className="products-container">
+      <div className="products-header d-flex justify-content-between align-items-center mb-3">
+        <span className="fs-5 text-uppercase fw-semibold">Products</span>
+        <Link
+          to={"/admin/products/create"}
+          className="btn btn-dark table-text fw-semibold"
+        >
+          New product
+        </Link>
+      </div>
+      <table className="table table-hover text-center align-middle dashboard-table rounded rounded-3 overflow-hidden shadow-sm table-text">
+        <thead className="table-header">
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col" className="text-start">
+              Name
+            </th>
+            <th scope="col" className="text-start">
+              Price
+            </th>
+            <th scope="col">Stock</th>
+            <th scope="col">Featured</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr key={product.id}>
+              <td>{product.id}</td>
+              <td className="text-start">{product.name} </td>
+
+              <td className="text-start">
+                {product.currency} {product.price}
+              </td>
+              <td>{product.stock}</td>
+              <td>
+                <i
+                  className={
+                    product.featured
+                      ? "bi bi-check-circle text-success"
+                      : "bi bi-x-circle text-danger"
+                  }
+                ></i>
+              </td>
+              <td>
+                <div className="d-flex justify-content-center gap-3 align-items-center">
+                  <i className="bi bi-pencil-square fs-6 edit-icon text-primary"></i>
+                  <i
+                    className="bi bi-trash3-fill fs-6 delete-icon text-danger"
+                    onClick={handleShowRemove}
+                  ></i>
+                  <RemoveModal
+                    show={showRemove}
+                    handleClose={handleCloseRemove}
+                    item={product.name}
+                    handleOnClick={handleRemoveProduct}
+                  />
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Products;
