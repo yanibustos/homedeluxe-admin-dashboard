be8bcd069bbf9c09caac7d5875969a5904dc6532
Yanina Bustos <yanibustos4596@gmail.com>
2025-04-07 01:28:46 -0300

    AddProduct - Add reusable form, change container to fluid, adjust test data and redirection/error handling

--- src/assets/pages/AddProduct/AddProduct.jsx
+++ src/assets/pages/AddProduct/AddProduct.jsx
@@ -1,19 +1,21 @@
 import "./AddProduct.css";
 import Input from "../../components/commons/Input/Input";
 import InputCheckbox from "../../components/commons/InputCheckbox/InputCheckbox";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import fetchApi from "../../../api/fetchApi";
 import { toast } from "react-toastify";
+import ProductForm from "../../components/ProductForm/ProductForm";
 
+//TODO: Add the right ids when relationship and model is defined in API
 const categories = [
-  { id: 1, name: "Sofas and armchairs" },
-  { id: 2, name: "Coffee Tables" },
+  { id: "Sofas and armchairs", name: "Sofas and armchairs" },
+  { id: "Coffee Tables", name: "Coffee Tables" },
 ];
 
-const currency = [{ id: 1, name: "USD" }];
+const currency = [{ id: "USD", name: "USD" }];
 
 const schema = yup
   .object({
@@ -24,6 +26,7 @@ const schema = yup
     category: yup.string().required("Category is required"),
     currency: yup.string().required("Currency is required"),
     featured: yup.boolean(),
+    //TODO: Check image validation
     // image: yup.mixed().test("fileType", "Unsupported file format", (value) => {
     //   if (!value) return true;
     //   return ["image/jpeg", "image/png", "image/webp", "image/jpg"].includes(
@@ -34,6 +37,8 @@ const schema = yup
   .required();
 
 function AddProduct() {
+  const navigate = useNavigate();
+
   const {
     register,
     handleSubmit,
@@ -47,15 +52,9 @@ function AddProduct() {
     mode: "onSubmit",
   });
 
-  console.log(errors);
-
   const onSubmit = async (data) => {
-    console.log("Estoy en onsubmit");
-    console.log({ data });
-    console.log(data.image);
     try {
       const formData = new FormData();
-      console.log({ formData });
 
       // Append each field to the FormData object
       formData.append("name", data.name);
@@ -78,7 +77,12 @@ function AddProduct() {
       });
 
       if (response) {
-        toast.success("Account created successfully, redirecting to Login...");
+        toast.success(
+          "Product created successfully, redirecting to Products view"
+        );
+        setTimeout(() => {
+          navigate("/admin/products");
+        }, 4000);
       }
     } catch (error) {
       toast.error("Something went wrong, please try again");
@@ -86,123 +90,18 @@ function AddProduct() {
   };
 
   return (
-    <div className="addProduct-container container">
+    <div className="addProduct-container container-fluid">
       <div className="addProduct-header fs-5 text-uppercase fw-semibold mb-3">
         Add product
       </div>
-      <form action="" onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <Input
-            type="text"
-            id="name"
-            name="name"
-            label="Name"
-            classNameLabel="fw-semibold"
-            register={{ ...register("name") }}
-            errors={errors}
-          />
-        </div>
-        <div className="my-3">
-          <label
-            htmlFor="description"
-            className="form-label fw-semibold addProduct-label"
-          >
-            Description
-          </label>
-          <textarea
-            id="description"
-            name="description"
-            rows="3"
-            {...register("description")}
-          ></textarea>
-          {errors.description && (
-            <p className="text-danger">{errors.description.message}</p>
-          )}
-        </div>
-        <div className="my-3">
-          <label htmlFor="image" className="fw-semibold addProduct-label mb-2">
-            Photos
-          </label>
-          <input
-            type="file"
-            id="image"
-            name="image"
-            className="form-control"
-            {...register("image")}
-          />
-        </div>
-        {errors.image && <p className="text-danger">{errors.image.message}</p>}
-        <div className="my-3">
-          <select
-            name="category"
-            id="category"
-            className="form-select"
-            {...register("category")}
-          >
-            <option value="">Choose a category</option>
-            {categories.map((category) => (
-              <option key={category.id} value={category.id}>
-                {category.name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="my-3">
-          <select
-            name="currency"
-            id="currency"
-            className="form-select"
-            {...register("currency")}
-          >
-            <option value="">Choose currency</option>
-            {currency.map((item) => (
-              <option key={item.id} value={item.id}>
-                {item.name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <Input
-            type="text"
-            id="price"
-            name="price"
-            label="Price"
-            classNameLabel="fw-semibold"
-            register={{ ...register("price") }}
-            errors={errors}
-          />
-        </div>
-        <div>
-          <Input
-            type="number"
-            id="stock"
-            name="stock"
-            label="Stock"
-            classNameLabel="fw-semibold"
-            register={{ ...register("stock") }}
-            errors={errors}
-          />
-        </div>
-        <div className="my-3">
-          <InputCheckbox
-            name="featured"
-            label="Is a featured product?"
-            id="featured"
-            labelClassName="fw-semibold addProduct-label"
-            register={{ ...register("featured") }}
-            errors={errors}
-          />
-        </div>
-        <div className="my-4">
-          <Link to={"/admin/products"} className="btn btn-secondary me-3">
-            Cancel
-          </Link>
-          <button type="submit" className="btn btn-dark">
-            Save
-          </button>
-        </div>
-      </form>
+      <ProductForm
+        register={register}
+        errors={errors}
+        currency={currency}
+        categories={categories}
+        onSubmit={onSubmit}
+        handleSubmit={handleSubmit}
+      />
     </div>
   );
 }
