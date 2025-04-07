3822d41709422167c2546fff03f0534dced41a3d
Yanina Bustos <yanibustos4596@gmail.com>
2025-04-07 01:26:38 -0300

    Loading - Add content and styles

--- src/assets/components/Loading/Loading.jsx
+++ src/assets/components/Loading/Loading.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Loading() {
   return (
-    <div class="d-flex justify-content-center vh-100">
-      <div class="spinner-border" role="status">
-        <span class="visually-hidden">Loading...</span>
+    <div className="d-flex justify-content-center vh-100">
+      <div className="spinner-border" role="status">
+        <span className="visually-hidden">Loading...</span>
       </div>
     </div>
   );
