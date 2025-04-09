0a6b4043ea0038bc8f17f127eb2ca8ec0768199c
Yanina Bustos <yanibustos4596@gmail.com>
2025-04-09 14:37:27 -0300

    ProtectedRoute - Change in Layout and Protected Route

--- src/assets/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/assets/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
+import Layout from "../Layout/Layout";
 
 function ProtectedRoute() {
   const user = useSelector((state) => state.user);
 
-  return user.accessToken ? <Outlet /> : <Navigate to="/login" />;
+  return user.accessToken ? (
+    <Layout>
+      <Outlet />
+    </Layout>
+  ) : (
+    <Navigate to="/admin/login" />
+  );
 }
 
 export default ProtectedRoute;
