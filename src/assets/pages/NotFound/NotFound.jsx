dacadcff0315596970e3a6abe5f7893b17f55899
Yanina Bustos <yanibustos4596@gmail.com>
2025-04-09 14:02:35 -0300

    NotFound - Add component and styles

--- src/assets/pages/NotFound/NotFound.jsx
+++ src/assets/pages/NotFound/NotFound.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+import "./NotFound.css";
 function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className="not-found-container container d-flex justify-content-center align-items-center flex-column">
+      <div className="not-found-card d-flex justify-content-center align-items-center flex-column">
+        <h1>404</h1>
+        <p>Sorry, we couldn't find this page</p>
+        <Link to={"/admin"} className="button">
+          Go back to Admin
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
