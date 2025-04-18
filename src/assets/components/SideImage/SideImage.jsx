747323e3538bbc6fbd8f5c4533867d8d5d99b96c
Yanina Bustos <yanibustos4596@gmail.com>
2025-04-18 20:02:17 -0300

    SideImage - Change admin login design to differentiate from user login

--- src/assets/components/SideImage/SideImage.jsx
+++ src/assets/components/SideImage/SideImage.jsx
@@ -2,13 +2,18 @@ import "./SideImage.css";
 
 function SideImage({ children }) {
   return (
-    <div className="sideImage-container container-fluid bg-white h-100">
-      <div className="row h-100 d-flex">
-        <div className="d-none d-xl-flex col-xl-9 bg-dark sideImage-div"></div>
-        <div className="col-12 col-xl-3 d-flex flex-column justify-content-center align-items-center">
-          <div className="vh-100 w-100 d-flex flex-column justify-content-center form-center">
-            <div className="d-flex justify-content-center align-items-center mb-4">
-              <img src="/img/logo_principal.png" alt="Logo" className="img-fluid" />
+    <div className="sideImage-container container-fluid h-100">
+      <div className="row h-100">
+        <div className="sideImage-div col-12"></div>
+        <div className="content d-flex flex-column justify-content-center align-items-center">
+          <div className="glass-card p-4">
+            <div className="d-flex flex-column justify-content-center align-items-center mb-4">
+              <img
+                src="/img/logo-white.png"
+                alt="Logo"
+                className="img-fluid mb-2"
+              />
+              <span className="text-white text-uppercase">Admin Access</span>
             </div>
             {children}
           </div>
